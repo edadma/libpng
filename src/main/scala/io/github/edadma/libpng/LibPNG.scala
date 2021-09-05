cb7028538a8c3c2bc768babdec3d592f186f67d3
Edward A Maxedon, Sr <edadma@gmail.com>
2021-09-05 13:25:34 -0400

    start coding functions

---
+++ src/main/scala/io/github/edadma/libpng/LibPNG.scala
@@ -0,0 +1,16 @@
+package io.github.edadma.libpng
+
+import scala.scalanative.unsafe._
+
+@link("png")
+@extern
+object LibPNG {
+
+  type png_const_bytep = Ptr[Byte]
+  type png_uint_32     = CUnsignedInt
+
+  def png_access_version_number: CUnsignedLong = extern
+
+  def png_sig_cmp(sig: png_const_bytep, start: CSize, num_to_check: CSize): CInt = extern
+
+}
---
+++ src/main/scala/io/github/edadma/libpng/package.scala
@@ -0,0 +1,37 @@
+package io.github.edadma
+
+import io.github.edadma.libpng.{LibPNG => lib}
+
+import scala.collection.mutable
+import scala.scalanative.unsafe._
+import scala.scalanative.unsigned._
+
+package object libpng {
+
+  private def copy(src: collection.Seq[Byte], dst: Ptr[Byte], count: UInt): Unit = {
+    for (i <- 0 until count.toInt)
+      dst(i) = src(i)
+  }
+
+  private def copy(src: Ptr[Byte], dst: mutable.Seq[Byte], count: Int): Unit = {
+    for (i <- 0 until count)
+      dst(i) = src(i)
+  }
+
+  private def bool(a: CInt): Boolean = if (a == 0) false else true
+
+  def png_access_version_number: String = {
+    val v = lib.png_access_version_number.toInt
+
+    s"${v / 10000}.${(v % 10000) / 100}.${v % 100}"
+  }
+
+  def png_sig_cmp(sig: collection.Seq[Byte]): Boolean = {
+    val count = sig.length min 8 toUInt
+    val a     = stackalloc[Byte](count)
+
+    copy(sig, a, count)
+    bool(lib.png_sig_cmp(a, 0.toUInt, count.toUInt))
+  }
+
+}
