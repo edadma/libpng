a7745286af6b2e8d4a231c539fe65c560179238f
Edward A Maxedon, Sr <edadma@gmail.com>
2021-09-05 19:41:37 -0400

    cont coding functions

--- src/main/resources/scala-native/macros.c
+++ src/main/resources/scala-native/macros.c
@@ -1,3 +1,4 @@
 #include <png.h>
+#include <setjmp.h>
 
 char* png_PNG_LIBPNG_VER_STRING() {return PNG_LIBPNG_VER_STRING; }
--- src/main/scala/io/github/edadma/libpng/extern/LibPNG.scala
+++ src/main/scala/io/github/edadma/libpng/extern/LibPNG.scala
@@ -7,6 +7,7 @@ import scala.scalanative.unsafe._
 @extern
 object LibPNG {
 
+  type png_byte           = CUnsignedChar
   type png_const_bytep    = Ptr[Byte]
   type png_uint_32        = CUnsignedInt
   type png_struct         = CStruct0
@@ -19,25 +20,26 @@ object LibPNG {
   type png_error_ptr      = CFuncPtr2[png_structp, png_const_charp, Unit]
   type png_info           = CStruct0
   type png_infop          = Ptr[png_info]
+  type png_const_inforp   = Ptr[png_info]
   type png_FILE_p         = Ptr[FILE]
 
-  def png_access_version_number: CUnsignedLong = extern
-
+  def png_access_version_number: CUnsignedLong                                   = extern
+  def png_set_sig_bytes(png_ptr: png_structrp, num_bytes: CInt): Unit            = extern
   def png_sig_cmp(sig: png_const_bytep, start: CSize, num_to_check: CSize): CInt = extern
-
   def png_create_read_struct(user_png_ver: CString,
                              error_ptr: png_voidp,
                              error_fn: png_error_ptr,
                              warn_fn: png_error_ptr): png_structp = extern
-
   def png_create_write_struct(user_png_ver: CString,
                               error_ptr: png_voidp,
                               error_fn: png_error_ptr,
-                              warn_fn: png_error_ptr): png_structp = extern
-
-  def png_get_compression_buffer_size(png_ptr: png_const_structrp): CSize = extern
-
-  def png_set_compression_buffer_size(png_ptr: png_const_structrp, size: CSize): Unit = extern
+                              warn_fn: png_error_ptr): png_structp                        = extern
+  def png_get_compression_buffer_size(png_ptr: png_const_structrp): CSize                 = extern
+  def png_set_compression_buffer_size(png_ptr: png_const_structrp, size: CSize): Unit     = extern
+  def png_create_info_struct(png_ptr: png_const_structrp): png_infop                      = extern
+  def png_read_info(png_ptr: png_structrp, info_ptr: png_infop): Unit                     = extern
+  def png_init_io(png_ptr: png_structrp, fp: png_FILE_p): Unit                            = extern
+  def png_get_channels(png_ptr: png_const_structrp, info_ptr: png_const_inforp): png_byte = extern
 
   // header macros
 
